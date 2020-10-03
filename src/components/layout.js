f5f628197b06dd606153037e079507f1807a7e30
github username <lushiyun@outlook.com>
2020-10-03 19:05:54 -0400

    Initial setup with plugins

---
+++ src/components/layout.js
@@ -0,0 +1,9 @@
+import React from "react"
+
+import './layout.css'
+import "typeface-roboto-mono"
+import "typeface-lora"
+
+const Layout = ({ children }) => <div>{children}</div>
+
+export default Layout
