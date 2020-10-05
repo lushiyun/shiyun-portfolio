1e68d0e8d9b1cd0c1749cc92e2296040f015009e
github username <lushiyun@outlook.com>
2020-10-05 16:18:44 -0400

    Add PWA and SEO configurations

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,13 @@
 const path = require(`path`)
 
 module.exports = {
+  siteMetadata: {
+    title: `Shiyun Lu Developer`,
+    description: `Shiyun Lu's portfolio site on frontend development and software engineering`,
+    author: `lushiyun`,
+    twitterUsername: `@lu_shiyun`,
+    siteUrl: `https://shiyunlu.com`
+  },
   plugins: [
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
@@ -21,5 +28,19 @@ module.exports = {
         contentTypes: [`projects`],
       },
     },
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `ShiyunLu`,
+        short_name: `SL`,
+        start_url: `/`,
+        background_color: `rgba(249, 220, 36, 0.4)`,
+        theme_color: `#f9dc24`,
+        display: `standalone`,
+        icon: `src/images/icon.png`,
+      },
+    },
+    `gatsby-plugin-offline`,
+    `gatsby-plugin-react-helmet`,
   ],
 }
