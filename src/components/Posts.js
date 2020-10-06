579227af560727e110df5b3bb8c50dbf7e5b644c
github username <lushiyun@outlook.com>
2020-10-06 00:44:21 -0400

    Wrap post card in href

--- src/components/Posts.js
+++ src/components/Posts.js
@@ -9,20 +9,20 @@ const Posts = () => {
     <section>
       <TitleLeft title={"Featured Posts"} anchor={"blog"} />
       <div className={styles.wrapper}>
-        <article className={styles.card}>
-          <div className={styles.cover}>
-            <img src={cover} alt="React and Redux logos" />
-          </div>
-          <div className={styles.body}>
-            <h3>
-              <a
-                href="https://medium.com/@lushiyun/action-cable-react-hooks-redux-toolkit-yet-another-chat-app-with-unread-messages-feature-93f5f36d4489?sk=ce3f4d7d1f9c4497dff9250d0bce9a29"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
+        <a
+          href="https://medium.com/@lushiyun/action-cable-react-hooks-redux-toolkit-yet-another-chat-app-with-unread-messages-feature-93f5f36d4489?sk=ce3f4d7d1f9c4497dff9250d0bce9a29"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <article className={styles.card}>
+            <div className={styles.cover}>
+              <img src={cover} alt="React and Redux logos" />
+            </div>
+            <div className={styles.body}>
+              <h3>
                 Action Cable, React Hooks, Redux Toolkit: Yet Another Chat App
-              </a>
-            </h3>
+              </h3>
+            </a>
             <p>
               <span className={styles.emoji}>👏</span> 206
             </p>
